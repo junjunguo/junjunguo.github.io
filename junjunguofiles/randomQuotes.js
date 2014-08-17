f5428bedd1e6a5f89f87a512dc5fbcce5bae21a8
Guo Junjun <guojunjun@gmail.com>
2014-08-18 00:01:24 +0200

    my quotes

---
+++ junjunguofiles/randomQuotes.js
@@ -0,0 +1,26 @@
+/**
+ * Created by junjun on 17.08.14.
+ */
+
+var textFile = readTextFile("../junjunguofiles/myQuotes.txt")
+
+console.log(textFile);
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
