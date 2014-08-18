975bf3d63aa8bfa527e10151c100c4b15d1d2f13
Guo Junjun <guojunjun@gmail.com>
2014-08-18 10:08:15 +0200

    add onclick change quotes

--- junjunguofiles/randomQuotes.js
+++ junjunguofiles/randomQuotes.js
@@ -6,7 +6,8 @@ var textFile = 'junjunguofiles/myQuotes.txt';
 
 console.log(textFile);
 
-
+var quotesArray = [];
+var totalQuotes = 0;
 var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
 
 function loadFile() {
@@ -15,17 +16,41 @@ function loadFile() {
     reader.send(null);
 }
 
+
 function displayContents() {
     if (reader.readyState == 4) {
-        console.log(reader.responseText);
-        var quotesArray = reader.responseText.split("\n");
-        var totalQuotes = quotesArray.length;
-        var randomQuotes = Math.floor(Math.random() * totalQuotes);
-        console.log("................"+quotesArray[0]);
-        console.log("total Quotes: "+totalQuotes+" random quotes: "+randomQuotes);
-
-        var el = document.getElementById('myquotes');
-        el.innerHTML = quotesArray[randomQuotes];
+        quotesArray = reader.responseText.split("\n");
+        totalQuotes = quotesArray.length;
+        //console.log(reader.responseText);
+        console.log("quotes array length: " + quotesArray.length);
+        console.log("if generate quotes....");
+        generateQuotes();
+    } else {
+        quotesArray = "Wonder is the beginning of wisdom.    <br>Socrates";
+        totalQuotes = quotesArray.length;
+        console.log("else: generate quotes....");
+        generateQuotes();
     }
 }
+
+
 loadFile();
+
+function generateQuotes() {
+    var randomQuotes = Math.floor(Math.random() * totalQuotes);
+    //console.log("................"+quotesArray[0]);
+    console.log("total Quotes: " + totalQuotes + " random quotes: " + randomQuotes);
+    var el = document.getElementById('myquotes');
+    el.innerHTML = quotesArray[randomQuotes];
+}
+
+
+var changeImageP = document.getElementById("prevslide");
+changeImageP.onclick = function () {
+    generateQuotes();
+}
+
+var changeImageN = document.getElementById("nextslide");
+changeImageN.onclick = function () {
+    generateQuotes();
+}
