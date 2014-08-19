08c62114599971758ecd86ca977f2dfe092b7842
Guo Junjun <guojunjun@gmail.com>
2014-08-19 22:36:51 +0200

    quotations

---
+++ junjunguofiles/quotes.js
@@ -0,0 +1,53 @@
+/**
+ * Created by junjun on 19.08.14.
+ */
+
+var textFile = 'junjunguofiles/quotations.txt';
+
+var quotesArray = [];
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+
+function loadFile() {
+    reader.open('get', textFile, true);
+    reader.onreadystatechange = displayContents;
+    reader.send(null);
+}
+
+
+function displayContents() {
+    if (reader.readyState == 4) {
+        quotesArray = reader.responseText.split("\n");
+        totalQuotes = quotesArray.length;
+        console.log("quotes array length: " + quotesArray.length);
+        generateQuotes();
+    } else {
+        quotesArray = "Wonder is the beginning of wisdom.    <br>Socrates";
+        totalQuotes = quotesArray.length;
+        console.log("else: generate quotes....");
+        //generateQuotes();
+    }
+}
+
+
+function generateQuotes() {
+    console.log("activ generate quotes: quotes array length: " + quotesArray.length);
+    for (var i = 0; i < quotesArray.length; i++) {
+        console.log(quotesArray[i]);
+        var theQuotation = quotesArray[i].split("#");
+        console.log(theQuotation.length);
+        var newElementL = document.createElement("li");
+        var newElementP = document.createElement("p");
+        var newElementH = document.createElement("h5");
+        //var quotationText = document.createTextNode(theQuotation[1]);
+        var quotationAuthor = document.createTextNode(" - "+theQuotation[0]);
+        // use innerHTML
+        //newElementP.appendChild(quotationText);
+        newElementP.innerHTML=theQuotation[1];
+        newElementH.appendChild(quotationAuthor);
+        newElementL.appendChild(newElementP);
+        newElementL.appendChild(newElementH);
+
+        document.getElementById("quotes").appendChild(newElementL);
+    }
+}
+loadFile();
