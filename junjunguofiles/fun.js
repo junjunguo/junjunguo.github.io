388377649d8674dc746b1bd4a4660f8883aa66cf
Guo Junjun <guojunjun@gmail.com>
2014-12-15 17:43:05 +0100

    songci

--- junjunguofiles/fun.js
+++ junjunguofiles/fun.js
@@ -4,15 +4,24 @@
 
 var imglist = [
     '2b就2b呗，还结巴.jpg',
+    'Fast Food.jpg',
+    'Its not a bug, its a feature..jpg',
+    'The difference between women and men.jpg',
+    'Todays Funny Wearing.jpg',
+    'Wake Up.jpg',
+    'random number.png',
     '万恶的备胎.jpg',
+    '万恶的输入法.jpg',
     '为了这个男人.jpg',
     '主人，你过来，我保证不咬你.jpg',
     '主人，我美么.jpg',
     '乐呵乐呵哈.jpg',
     '人生是什么，恩？让我想想.jpg',
+    '代码猿的痛.jpg',
     '你个骗纸！！！姿势简直太标准.gif',
     '你是怎么怀孕的.jpg',
     '你肚吾肚饿啊，我煮碗面俾你食啦~.jpg',
+    '你这是在自己玩么.gif',
     '健身后的效果.jpg',
     '凌云飞渡笑傲江湖.gif',
     '勤劳的小松鼠.jpg',
@@ -20,6 +29,7 @@ var imglist = [
     '南昌专列上车走了.jpg',
     '卧槽，不带这么玩的！.jpg',
     '原来”高攀不起“是这么来的!.gif',
+    '去日本最好的季节.jpg',
     '县级公路惊现飘移车神.gif',
     '同学！你男盆友掉了.jpg',
     '呵呵，交接班.jpg',
@@ -27,12 +37,17 @@ var imglist = [
     '哥们，算，算。。。算你很.gif',
     '哪来的蒙面大叔！？？.jpg',
     '唉，你说我怎么就学不会西红柿炒鸡蛋呢？！.jpg',
+    '啪啪啪....jpg',
     '嘿嘿，嘿喂够。电臀都起来.gif',
+    '坑爹的上帝.gif',
+    '大哥你东西掉了.jpg',
     '大师，给算算咋破啊.jpg',
     '大爷，您能低调点吗？.jpg',
     '女同事的双手，瞬间真相了.jpg',
     '妈，我心脏有点不舒服，先挂了.jpg',
     '孩子 昨天晚上你看到了什么.gif',
+    '小鸡的未来.jpg',
+    '屌丝的逆袭.gif',
     '屌丝福利，全世界美女都归你.jpg',
     '应变.jpg',
     '当学渣遇上考试时，往往是介样的。。。.jpg',
@@ -42,20 +57,29 @@ var imglist = [
     '想开了好，以后还长着呢.jpg',
     '想要看起来像个大人，于是我就帮了他。。.jpg',
     '我只想说：这样有加班费么？？.jpg',
+    '我太帅了.jpg',
     '我是一个装满眼泪的瓶子.jpg',
     '我有特殊的撬别人女朋友的技巧.gif',
+    '我来教你怎么走路.gif',
     '挤眉弄眼神马的最开心了.gif',
     '救人一命，胜造七级浮屠.jpg',
+    '每个人都是宇宙的中心.GIF',
+    '灰太狼失败的基本原因是别的狼都吃生的.jpg',
     '熊孩子，逗姥姥玩是吧！.gif',
+    '牛.gif',
     '牛叉！！.gif',
     '疯子和傻子.jpg',
+    '看到你的那一刻.jpg',
     '神回复.jpg',
     '第一遍看时并没有觉得哪里不对.jpg',
     '经济实惠的方式来解释男生女生之间的差异.jpg',
     '美女等一下.jpg',
     '老公还是你厉害.jpg',
+    '老妈，你不用这么犀利吧？.jpg',
+    '苦逼的程序员！.jpg',
     '萌哒哒滴！.jpg',
     '蛾眉大侠在此.jpg',
+    '请尊重这门专业.jpg',
     '这技术，屌爆了！.gif',
     '这棵树真大.jpg',
     '这眼神，这表白，妹子跟我走吧.jpg',
@@ -113,6 +137,7 @@ function addImage(theImg) {
 }
 
 function generateaJoke() {
+//    console.log("jokes: "+jokes.length)
     if (Math.floor((Math.random() * 10) % 2) == 0) { // 50% generate possibility
         var index = Math.floor((Math.random() * (jokes.length)));
         var ajoke = jokes.splice(index, 1);
@@ -127,12 +152,15 @@ function restJokes() {
     }
 }
 function addJoke(joke) {
-    if (joke[0]) {
+    if (joke[0].length > 2) {
         var litag = document.createElement("li");
         var htag = document.createElement("h6");
         htag.innerHTML = text2html(joke[0]);
         litag.appendChild(htag);
         document.getElementById("quotes").appendChild(litag);
+    }
+    else{
+//        console.log("enmpty: ["+joke[0] +"]")
     }
 }
 function text2html(str) {
