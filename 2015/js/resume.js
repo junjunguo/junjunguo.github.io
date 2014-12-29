e8c6fd9867240bd23d0dcb23d1b0dde2ee600a51
Guo Junjun <guojunjun@gmail.com>
2014-12-29 03:47:24 +0100

    my Curriculum vitea done!

--- 2015/js/resume.js
+++ 2015/js/resume.js
@@ -1,9 +1,9 @@
 /**
- * Created by junjun on 27.12.14.
+ * Created by GuoJunjun on 27.12.14. JunjunGuo.com
  */
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-//Timeline
+//item trigger
     $('.itemTrigger span').click(function () {
         if ($(this).hasClass('fa fa-plus')) {
             $(this).removeClass('fa fa-plus').addClass('fa fa-minus');
@@ -19,4 +19,12 @@ jQuery(document).ready(function($) {
             $trigger.removeClass('fa fa-minus').addClass('fa fa-plus');
         }
     });
+    // mouseover function
+    $('.movertrigger').mouseover(function () {
+        $(this).removeClass('ison').addClass('isoff');
+    })
+        .mouseout(function () {
+                      $(this).removeClass('isoff').addClass('ison');
+                  });
 });
+
