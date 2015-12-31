0f47852deff174ae6093243efd71f3b2fbf60401
Junjun Guo <guojunjun@gmail.com>
2015-12-31 02:13:28 +0100

    add wow animation

--- junjunguofiles/quotes.js
+++ junjunguofiles/quotes.js
@@ -5,7 +5,7 @@
 var textFile = 'junjunguofiles/quotations.txt';
 
 var quotesArray = [];
-var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+var reader      = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
 
 function loadFile() {
     reader.open('get', textFile, true);
@@ -35,12 +35,12 @@ function generateQuotes() {
         //console.log(jokes[i]);
         var theQuotation = quotesArray[i].split("#");
         //console.log(theQuotation.length);
-        var element_li = document.createElement("li");
-        var element_blockquote = document.createElement("blockquote");
-        //element_blockquote.className = element_blockquote.className + " blockquote-reverse";
-        var element_p = document.createElement("p");
-        var element_footer = document.createElement("footer");
-        var element_cite = document.createElement("cite");
+        var element_li               = document.createElement("li");
+        var element_blockquote       = document.createElement("blockquote");
+        element_blockquote.className = element_blockquote.className + " wow slideInRight";
+        var element_p                = document.createElement("p");
+        var element_footer           = document.createElement("footer");
+        var element_cite             = document.createElement("cite");
         //var quotationText = document.createTextNode(theQuotation[1]);
         var quotationAuthor = document.createTextNode(theQuotation[0]);
         // use innerHTML
@@ -56,3 +56,20 @@ function generateQuotes() {
     }
 }
 loadFile();
+
+// load js/wow.min.js
+var wow = new WOW(
+    {
+        boxClass:        'wow',      // animated element css class (default is wow)
+        animateClass:    'animated', // animation css class (default is animated)
+        offset:          0,          // distance to the element when triggering the animation (default is 0)
+        mobile:          true,       // trigger animations on mobile devices (default is true)
+        live:            true,       // act on asynchronously loaded content (default is true)
+        callback:        function (box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+);
+wow.init();
