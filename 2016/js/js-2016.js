fcfbeb4bf6f7a8a25ff573065dfdf6129f910285
Junjun Guo <guojunjun@gmail.com>
2016-02-29 02:57:15 +0100

    my web 2016: update 1

--- 2016/js/js-2016.js
+++ 2016/js/js-2016.js
@@ -3,6 +3,100 @@
  *
  */
 
+
+/**
+ * check if client from China
+ */
+    //function clientlocationplugin() {
+    //    return jQuery.getScript('http://www.geoplugin.net/javascript.gp', function () {
+    //        var country = geoplugin_countryName();
+    //        var zone = geoplugin_region();
+    //        var district = geoplugin_city();
+    //        console.log("Your location is: " + country + ", " + zone + ", " + district);
+    //    })();
+    //}
+//console.log("Your location: " + geoplugin_countryName());
+/**
+ *  * shuffle the list and return a photo addressed list
+ * @param list
+ * @returns {Array}
+ */
+function shuffleList(list) { // shuffle nrlist and return a list with objects ready for slides
+    var copy = [], n = list.length, index;
+    var bgimgaddress;
+    if (geoplugin_countryName() == "China") {
+        bgimgaddress = '../../2015/images/bg/';
+    } else {
+        bgimgaddress = 'http://static.panoramio.com/photos/large/';
+    }
+    while (n) {
+        index = Math.floor(Math.random() * (n--));
+        //        var overlay = 'url(".2015/css/images/overlay-black.svg"),'
+        copy.push(bgimgaddress + list[index] + '.jpg');
+        list.splice(index, 1);
+    }
+    return copy;
+}
+
+/**
+ * add the image to #header as background image
+ * @param theImg
+ */
+function addImage(theImg) {
+    document.getElementById("first-page").style.backgroundImage = "url('" + theImg + "')";
+}
+var listh = [			//  Images id numbers for header
+    77233663,
+    105836333,
+    109199045,
+    109172199,
+    92464442,
+    92464435,
+    107909681,
+    51569735,
+    108930464,
+    109049207,
+    109100961,
+    106419102,
+    51569745,
+    78491829,
+    49428516,
+    43143230,
+    106397984
+];
+function slideShow() {
+    var counter = 0; // to keep track of current slide
+    var slides = shuffleList(listh); // a collection of all of the slides
+
+    var showImage = function () {
+        // calculates the actual index of the element to show
+        var theImage = Math.abs(counter % slides.length);
+        addImage(slides[theImage]);
+    };
+
+    // add click events to prev &amp; next buttons
+    $('.nextslide').on('click', function () {
+        document.getElementById('header').scrollIntoView()
+        counter++;
+        showImage();
+    });
+    $('.prevslide').on('click', function () {
+        document.getElementById('header').scrollIntoView()
+        counter--;
+        showImage();
+    });
+    // logo click
+    $('#logo').on('click', function () {
+        document.getElementById('header').scrollIntoView()
+        counter = Math.floor(Math.random() * slides.length);
+        showImage();
+    });
+
+
+    showImage();
+}
+
+
 /**
  * set up full page : https://github.com/alvarotrigo/fullPage.js
  */
@@ -51,8 +145,8 @@ $(document).ready(function () {
         controlArrows:    true,
         verticalCentered: true,
         resize:           false,
-        sectionsColor:    ['#ccc',
-                           '#fff'],
+        //sectionsColor:    ['#ccc',
+        //                   '#fff'],
         paddingTop:       '3em',
         paddingBottom:    '10px',
         fixedElements:    '#header, .footer',
@@ -72,3 +166,9 @@ $(document).ready(function () {
         onSlideLeave:   function (anchorLink, index, slideIndex, direction, nextSlideIndex) {}
     });
 });
+
+
+//run the script:
+
+//run slide show
+slideShow();
